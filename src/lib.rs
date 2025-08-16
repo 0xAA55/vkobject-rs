d5906b6921c40deca24a9ad83762c8f21a26bb34
0xAA55 <weixiaobao213@qq.com>
2025-08-16 19:25:52 +0800

    添加一些公开的库

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 
+#![allow(unused_imports)]
+
+/// The VkCore initializer
+pub mod init;
+
+/// The common things for you to use
+pub mod prelude {
+	pub use vkcore_rs::*;
+	pub use crate::init::*;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
