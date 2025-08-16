f1ef2fd00e9e2adf085f105eb4757d294354ddb2
0xAA55 <weixiaobao213@qq.com>
2025-08-16 19:26:05 +0800

    实现一个最小的可运行单元测试。

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,42 @@ pub mod prelude {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use glfw::*;
+	use crate::prelude::*;
 
-    #[test]
-    fn test() {
-    }
+	const TEST_TIME: f64 = 10.0;
+
+	#[test]
+	fn test() {
+		let test_time: Option<f64> = Some(TEST_TIME);
+		let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+		let (mut window, events) = glfw.create_window(1024, 768, "GLFW Window", glfw::WindowMode::Windowed).expect("Failed to create VKFW window.");
+
+		window.set_key_polling(true);
+		window.make_current();
+		glfw.set_swap_interval(SwapInterval::Adaptive);
+
+		let vkcore = create_vkcore_from_glfw("VkObject-test", "VkObject-rs", vk_make_version(1, 0, 0), vk_make_version(1, 0, 0), vk_make_api_version(0, 1, 3, 0));
+
+		let start_time = glfw.get_time();
+		while !window.should_close() {
+			let cur_frame_time = glfw.get_time();
+
+			window.swap_buffers();
+			glfw.poll_events();
+			for (_, event) in glfw::flush_messages(&events) {
+				match event {
+					glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+						window.set_should_close(true)
+					}
+					_ => {}
+				}
+			}
+			if let Some(test_time) = test_time {
+				if cur_frame_time - start_time >= test_time {
+					window.set_should_close(true)
+				}
+			}
+		}
+	}
 }
