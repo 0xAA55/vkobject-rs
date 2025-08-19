d4beefdda820eb1c1a43a1fa37b31f4fdd913002
0xAA55 <weixiaobao213@qq.com>
2025-08-19 21:32:14 +0800

    准备实现对 Vulkan 的 Buffer 对象的管理。

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,4 +1,25 @@
 
 use crate::prelude::*;
+use std::{
+	cell::RefCell,
+	sync::{Arc, Weak},
+};
 
+#[derive(Debug)]
+pub struct Buffer {
+	pub states: Weak<RefCell<VulkanStates>>,
+	memory: VkDeviceMemory,
+	buffer: VkBuffer,
+}
 
+impl Buffer {
+	/// Get the buffer memory
+	pub fn get_memory(&self) -> VkDeviceMemory {
+		self.memory
+	}
+
+	/// Get the buffer
+	pub fn get_buffer(&self) ->VkBuffer {
+		self.buffer
+	}
+}
