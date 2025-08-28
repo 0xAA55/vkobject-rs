4e46a31649f733a7cc3eb65965a3df207b90d458
0xAA55 <weixiaobao213@qq.com>
2025-08-28 18:23:44 +0800

    保守治疗。只请求 Vulkan 1.0 的接口，因为现在很多集成显卡的驱动跟不上。

--- src/init.rs
+++ src/init.rs
@@ -32,7 +32,7 @@ pub mod init_from_glfw {
 
 	/// Create a Vulkan context
 	pub fn create_vulkan_context(window: &glfw::PWindow, vsync: bool, max_concurrent_frames: usize, is_vr: bool) -> Result<Arc<Mutex<VulkanContext>>, VulkanError> {
-		let vkcore = Arc::new(create_vkcore_from_glfw("VkObject-test", "VkObject-rs", vk_make_version(1, 0, 0), vk_make_version(1, 0, 0), vk_make_api_version(0, 1, 4, 0))?);
+		let vkcore = Arc::new(create_vkcore_from_glfw("VkObject-test", "VkObject-rs", vk_make_version(1, 0, 0), vk_make_version(1, 0, 0), vk_make_api_version(0, 1, 0, 0))?);
 		let device = VulkanDevice::choose_gpu_with_graphics(vkcore.clone(), max_concurrent_frames)?;
 		let surface = VulkanSurfaceInfo {
 			window,
