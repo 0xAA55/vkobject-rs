fa6446879c8cc4afdc0258db68a4b0ec4fd15941
0xAA55 <weixiaobao213@qq.com>
2025-08-30 15:18:54 +0800

    简化 `VulkanSurface` 的接口，使其不再依赖一个 `ctx: Weak<Mutex<VulkanContext>>` 并且也不需要再调用 `set_ctx()` 了，而是只需要依赖一个 `Arc<VkCore>` 即可做到 RAII 资源释放。

--- src/surface.rs
+++ src/surface.rs
@@ -3,13 +3,13 @@ use crate::prelude::*;
 use std::{
 	fmt::Debug,
 	ptr::{null, null_mut},
-	sync::{Mutex, Arc, Weak},
+	sync::Arc,
 };
 
 #[derive(Debug)]
 pub struct VulkanSurface {
-	/// The `VulkanContext` that helps to manage the `VkSurfaceKHR`
-	ctx: Weak<Mutex<VulkanContext>>,
+	/// The Vulkan driver
+	vkcore: Arc<VkCore>,
 
 	/// The handle to the surface
 	surface: VkSurfaceKHR,
@@ -22,24 +22,25 @@ unsafe impl Send for VulkanSurface {}
 
 impl VulkanSurface {
 	/// Create a new `VulkanSurface` by the given `VkSurfaceKHR` and `VkSurfaceFormatKHR`
-	pub fn new_from(surface: VkSurfaceKHR, format: VkSurfaceFormatKHR) -> Arc<Mutex<Self>> {
-		Arc::new(Mutex::new(Self {
-			ctx: Weak::new(),
+	pub fn new_from(vkcore: Arc<VkCore>, surface: VkSurfaceKHR, format: VkSurfaceFormatKHR) -> Self {
+		Self {
+			vkcore,
 			surface,
 			format,
-		}))
+		}
 	}
 	/// Create a new `VulkanSurface` from the generic type `surface_ci` by calling the given function pointer of `vkCreateSurfaceXxxx()`
 	#[allow(dead_code)]
-	fn new_from_ci<T>(function_name: &'static str, vkcore: &VkCore, device: &VulkanDevice, vk_create_surface: fn(VkInstance, &T, *const VkAllocationCallbacks, *mut VkSurfaceKHR) -> VkResult, surface_ci: &T) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	fn new_from_ci<T>(function_name: &'static str, vkcore: Arc<VkCore>, device: &VulkanDevice, vk_create_surface: fn(VkInstance, &T, *const VkAllocationCallbacks, *mut VkSurfaceKHR) -> VkResult, surface_ci: &T) -> Result<Self, VulkanError> {
 		let gpu_info = device.get_gpu();
 		let mut surface: VkSurfaceKHR = null();
 		vk_result_conv(function_name, vk_create_surface(vkcore.get_instance(), surface_ci, null(), &mut surface))?;
+		let surface = ResourceGuard::new(surface, |&s|{let vkcore = vkcore.clone(); vkcore.vkDestroySurfaceKHR(vkcore.get_instance(), s, null()).unwrap()});
 
 		let queue_families = gpu_info.get_queue_families();
 		let mut supported = Vec::<bool>::with_capacity(queue_families.len());
 		for i in 0..queue_families.len() {
-			supported.push(device.get_supported_by_surface(i, surface)?);
+			supported.push(device.get_supported_by_surface(i, *surface)?);
 		}
 		let mut graphics_queue_node_index = u32::MAX;
 		let mut present_queue_node_index = u32::MAX;
@@ -67,9 +68,9 @@ impl VulkanSurface {
 			return Err(VulkanError::NoGoodQueueForSurface("Separate graphics and presenting queues are not supported yet!"));
 		}
 		let mut num_formats: u32 = 0;
-		vkcore.vkGetPhysicalDeviceSurfaceFormatsKHR(gpu_info.get_vk_physical_device(), surface, &mut num_formats, null_mut())?;
+		vkcore.vkGetPhysicalDeviceSurfaceFormatsKHR(gpu_info.get_vk_physical_device(), *surface, &mut num_formats, null_mut())?;
 		let mut formats = Vec::<VkSurfaceFormatKHR>::with_capacity(num_formats as usize);
-		vkcore.vkGetPhysicalDeviceSurfaceFormatsKHR(gpu_info.get_vk_physical_device(), surface, &mut num_formats, formats.as_mut_ptr())?;
+		vkcore.vkGetPhysicalDeviceSurfaceFormatsKHR(gpu_info.get_vk_physical_device(), *surface, &mut num_formats, formats.as_mut_ptr())?;
 		unsafe {formats.set_len(num_formats as usize)};
 
 		let mut selected_format = formats[0];
@@ -90,16 +91,17 @@ impl VulkanSurface {
 				}
 			}
 		}
-		Ok(Self::new_from(surface, selected_format))
+		let surface = surface.release();
+		Ok(Self::new_from(vkcore, surface, selected_format))
 	}
 	/// Create a `VulkanSurface` by using the GLFW API
 	#[cfg(any(feature = "glfw", test))]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, window: &glfw::PWindow) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, window: &glfw::PWindow) -> Result<Self, VulkanError> {
 		Self::new_from_ci("vkCreateWindowSurfaceGLFW", vkcore, device, vkCreateWindowSurfaceGLFW, window)
 	}
 	/// Create a `VulkanSurface` by using the Win32 API
 	#[cfg(feature = "win32_khr")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, hwnd: HWND, hinstance: HINSTANCE) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, hwnd: HWND, hinstance: HINSTANCE) -> Result<Self, VulkanError> {
 		let surface_ci = VkWin32SurfaceCreateInfoKHR {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR,
 			pNext: null(),
@@ -111,7 +113,7 @@ impl VulkanSurface {
 	}
 	/// Create a `VulkanSurface` for Android
 	#[cfg(feature = "android_khr")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, window: *const ANativeWindow) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, window: *const ANativeWindow) -> Result<Self, VulkanError> {
 		let surface_ci = VkAndroidSurfaceCreateInfoKHR {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR,
 			pNext: null(),
@@ -122,7 +124,7 @@ impl VulkanSurface {
 	}
 	/// Create a `VulkanSurface` for IOS
 	#[cfg(feature = "ios_mvk")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, view: *const c_void) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, view: *const c_void) -> Result<Self, VulkanError> {
 		let surface_ci = VkIOSSurfaceCreateInfoMVK {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_IOS_SURFACE_CREATE_INFO_MVK,
 			pNext: null(),
@@ -133,7 +135,7 @@ impl VulkanSurface {
 	}
 	/// Create a `VulkanSurface` for MacOS
 	#[cfg(feature = "macos_mvk")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, view: *const c_void) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, view: *const c_void) -> Result<Self, VulkanError> {
 		let surface_ci = VkMacOSSurfaceCreateInfoMVK {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT,
 			pNext: null(),
@@ -144,7 +146,7 @@ impl VulkanSurface {
 	}
 	/// Create a `VulkanSurface` for Metal
 	#[cfg(feature = "metal_ext")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, metal_layer: *const CAMetalLayer) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, metal_layer: *const CAMetalLayer) -> Result<Self, VulkanError> {
 		let surface_ci = VkMetalSurfaceCreateInfoEXT {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT,
 			pNext: null(),
@@ -155,7 +157,7 @@ impl VulkanSurface {
 	}
 	/// Create a `VulkanSurface` for Wayland
 	#[cfg(feature = "wayland_khr")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, display: *const c_void, surface: *const c_void) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, display: *const c_void, surface: *const c_void) -> Result<Self, VulkanError> {
 		let surface_ci = VkWaylandSurfaceCreateInfoKHR {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_WAYLAND_SURFACE_CREATE_INFO_KHR,
 			pNext: null(),
@@ -167,7 +169,7 @@ impl VulkanSurface {
 	}
 	/// Create a `VulkanSurface` for XCB
 	#[cfg(feature = "xcb_khr")]
-	pub fn new(vkcore: &VkCore, device: &VulkanDevice, connection: *const c_void, window: xcb_window_t) -> Result<Arc<Mutex<Self>>, VulkanError> {
+	pub fn new(vkcore: Arc<VkCore>, device: &VulkanDevice, connection: *const c_void, window: xcb_window_t) -> Result<Self, VulkanError> {
 		let surface_ci = VkXcbSurfaceCreateInfoKHR {
 			sType: VkStructureType::VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR,
 			pNext: null(),
@@ -176,11 +178,6 @@ impl VulkanSurface {
 			window,
 		};
 		Self::new_from_ci("vkCreateXcbSurfaceKHR", vkcore, device, vkCreateXcbSurfaceKHR, &surface_ci)
-	}
-
-	/// Set the `VulkanContext` if it hasn't provided previously
-	pub(crate) fn set_ctx(&mut self, ctx: Weak<Mutex<VulkanContext>>) {
-		self.ctx = ctx;
 	}
 
 	/// Get the current `VkSurfaceKHR`
@@ -196,10 +193,6 @@ impl VulkanSurface {
 
 impl Drop for VulkanSurface {
 	fn drop(&mut self) {
-		if let Some(binding) = self.ctx.upgrade() {
-			let ctx = binding.lock().unwrap();
-			let vkcore = ctx.get_vkcore();
-			vkcore.vkDestroySurfaceKHR(vkcore.get_instance(), self.surface, null()).unwrap();
-		}
+		self.vkcore.vkDestroySurfaceKHR(self.vkcore.get_instance(), self.surface, null()).unwrap();
 	}
 }
