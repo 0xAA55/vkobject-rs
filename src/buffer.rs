e5165d70261c7296bb7df28b01bf307d16acc577
0xAA55 <weixiaobao213@qq.com>
2025-08-30 11:24:35 +0800

    实现对 `VkBuffer` 的封装。

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,23 +1,66 @@
 
 use crate::prelude::*;
 use std::{
-	cell::RefCell,
-	sync::Weak,
+	ffi::c_void,
+	ptr::{null, null_mut, copy},
+	mem::MaybeUninit,
+	sync::{Arc, Mutex},
 };
 
+/// The buffer object that temporarily stores the `VkBuffer`
 #[derive(Debug)]
-pub struct Buffer {
+struct Buffer {
 	/// The `VulkanContext` that helps to manage the resources of the buffer
-	ctx: Weak<RefCell<VulkanContext>>,
-
-	/// The handle to the device memory
-	memory: VkDeviceMemory,
+	ctx: Arc<Mutex<VulkanContext>>,
 
 	/// The handle to the buffer
 	buffer: VkBuffer,
 }
 
 impl Buffer {
+	/// Create the `Buffer`
+	fn new(ctx: Arc<Mutex<VulkanContext>>, size: usize, usage: VkBufferUsageFlags) -> Result<Self, VulkanError> {
+		let lock = ctx.lock().unwrap();
+		let vkcore = lock.get_vkcore();
+		let vkdevice = lock.get_vk_device();
+		let buffer_ci = VkBufferCreateInfo {
+			sType: VkStructureType::VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+			pNext: null(),
+			flags: 0,
+			size: size as VkDeviceSize,
+			usage,
+			sharingMode: VkSharingMode::VK_SHARING_MODE_EXCLUSIVE,
+			queueFamilyIndexCount: 0,
+			pQueueFamilyIndices: null(),
+		};
+		let mut buffer: VkBuffer = null();
+		vkcore.vkCreateBuffer(vkdevice, &buffer_ci, null(), &mut buffer)?;
+		drop(lock);
+		Ok(Self {
+			ctx,
+			buffer,
+		})
+	}
+
+	fn get_memory_requirements(&self) -> Result<VkMemoryRequirements, VulkanError> {
+		let lock = self.ctx.lock().unwrap();
+		let vkcore = lock.get_vkcore();
+		let vkdevice = lock.get_vk_device();
+		let mut ret: VkMemoryRequirements = unsafe {MaybeUninit::zeroed().assume_init()};
+		vkcore.vkGetBufferMemoryRequirements(vkdevice, self.buffer, &mut ret)?;
+		Ok(ret)
+	}
+}
+
+impl Drop for Buffer {
+	fn drop(&mut self) {
+		let lock = self.ctx.lock().unwrap();
+		let vkcore = lock.get_vkcore();
+		let vkdevice = lock.get_vk_device();
+		vkcore.vkDestroyBuffer(vkdevice, self.buffer, null()).unwrap();
+	}
+}
+
 	/// Get the buffer memory
 	pub fn get_memory(&self) -> VkDeviceMemory {
 		self.memory
