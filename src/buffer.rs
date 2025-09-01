64b4ca399f3447d293dcef42ea80d7514d8b5055
0xAA55 <weixiaobao213@qq.com>
2025-09-01 19:41:09 +0800

    把底层的 `VkBuffer` 的封装的结构体改名为 `VulkanBuffer`，而把之前的 `VulkanBuffer` 再改名为 `Buffer` 以体现抽象度。

--- src/buffer.rs
+++ src/buffer.rs
@@ -9,7 +9,7 @@ use std::{
 };
 
 /// The buffer object that temporarily stores the `VkBuffer`
-struct Buffer {
+pub struct VulkanBuffer {
 	/// The `VulkanDevice` is the associated device
 	pub device: Arc<VulkanDevice>,
 
@@ -17,9 +17,9 @@ struct Buffer {
 	buffer: VkBuffer,
 }
 
-impl Buffer {
-	/// Create the `Buffer`
-	fn new(device: Arc<VulkanDevice>, size: usize, usage: VkBufferUsageFlags) -> Result<Self, VulkanError> {
+impl VulkanBuffer {
+	/// Create the `VulkanBuffer`
+	pub fn new(device: Arc<VulkanDevice>, size: usize, usage: VkBufferUsageFlags) -> Result<Self, VulkanError> {
 		let vkcore = device.vkcore.clone();
 		let vkdevice = device.get_vk_device();
 		let buffer_ci = VkBufferCreateInfo {
@@ -40,7 +40,7 @@ impl Buffer {
 		})
 	}
 
-	fn get_memory_requirements(&self) -> Result<VkMemoryRequirements, VulkanError> {
+	pub fn get_memory_requirements(&self) -> Result<VkMemoryRequirements, VulkanError> {
 		let vkcore = self.device.vkcore.clone();
 		let mut ret: VkMemoryRequirements = unsafe {MaybeUninit::zeroed().assume_init()};
 		vkcore.vkGetBufferMemoryRequirements(self.device.get_vk_device(), self.buffer, &mut ret)?;
@@ -52,15 +52,15 @@ impl Buffer {
 	}
 }
 
-impl Debug for Buffer {
+impl Debug for VulkanBuffer {
 	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-		f.debug_struct("Buffer")
+		f.debug_struct("VulkanBuffer")
 		.field("buffer", &self.buffer)
 		.finish()
 	}
 }
 
-impl Drop for Buffer {
+impl Drop for VulkanBuffer {
 	fn drop(&mut self) {
 		let vkcore = self.device.vkcore.clone();
 		vkcore.vkDestroyBuffer(self.device.get_vk_device(), self.buffer, null()).unwrap();
@@ -68,27 +68,28 @@ impl Drop for Buffer {
 }
 
 /// The Vulkan buffer object, same as the OpenGL buffer object, could be used to store vertices, elements(indices), and the other data.
-pub struct VulkanBuffer {
+pub struct Buffer {
 	/// The `VulkanDevice` is the associated device
 	pub device: Arc<VulkanDevice>,
 
 	/// The device memory
-	memory: VulkanMemory,
+	pub memory: VulkanMemory,
 
 	/// The buffer
-	buffer: Buffer,
+	pub buffer: VulkanBuffer,
 }
 
-impl VulkanBuffer {
+impl Buffer {
+	/// Create a new 
 	pub fn new(device: Arc<VulkanDevice>, size: usize, data: *const c_void, usage: VkBufferUsageFlags, queue_index: usize) -> Result<Self, VulkanError> {
 		let vkcore = device.vkcore.clone();
-		let staging_buffer = Buffer::new(device.clone(), size, VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_SRC_BIT as u32)?;
+		let staging_buffer = VulkanBuffer::new(device.clone(), size, VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_SRC_BIT as u32)?;
 		let staging_memory = VulkanMemory::new(device.clone(), &staging_buffer.get_memory_requirements()?,
 			VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT as u32 |
 			VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_COHERENT_BIT as u32)?;
 		staging_memory.bind_buffer(staging_buffer.get_vk_buffer())?;
 		staging_memory.set_data(data)?;
-		let buffer = Buffer::new(device.clone(), size, usage | VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_DST_BIT as u32)?;
+		let buffer = VulkanBuffer::new(device.clone(), size, usage | VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_DST_BIT as u32)?;
 		let memory = VulkanMemory::new(device.clone(), &buffer.get_memory_requirements()?,
 			VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT as u32)?;
 		memory.bind_buffer(buffer.get_vk_buffer())?;
@@ -109,21 +110,11 @@ impl VulkanBuffer {
 			buffer,
 		})
 	}
-
-	/// Get the buffer memory
-	pub(crate) fn get_vk_memory(&self) -> VkDeviceMemory {
-		self.memory.get_vk_memory()
-	}
-
-	/// Get the buffer
-	pub(crate) fn get_vk_buffer(&self) -> VkBuffer {
-		self.buffer.buffer
-	}
 }
 
-impl Debug for VulkanBuffer {
+impl Debug for Buffer {
 	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-		f.debug_struct("VulkanBuffer")
+		f.debug_struct("Buffer")
 		.field("memory", &self.memory)
 		.field("buffer", &self.buffer)
 		.finish()
