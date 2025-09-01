76446bf960c73bf14db3a75ba46a7f7336a217a4
0xAA55 <weixiaobao213@qq.com>
2025-09-01 20:12:38 +0800

    将初始化缓冲区和设置数据的部分的代码解耦，并且不再保证初始化完事儿后一定就能保证数据上传完了。

--- src/buffer.rs
+++ src/buffer.rs
@@ -23,39 +23,25 @@ pub struct Buffer {
 
 impl Buffer {
 	/// Create a new buffer
-	pub fn new(device: Arc<VulkanDevice>, size: VkDeviceSize, data: *const c_void, usage: VkBufferUsageFlags, queue_index: usize) -> Result<Self, VulkanError> {
-		let vkcore = device.vkcore.clone();
-		let staging_buffer = VulkanBuffer::new(device.clone(), size, VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_SRC_BIT as u32)?;
-		let staging_memory = VulkanMemory::new(device.clone(), &staging_buffer.get_memory_requirements()?,
-			VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT as u32 |
-			VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_COHERENT_BIT as u32)?;
-		staging_memory.bind_buffer(staging_buffer.get_vk_buffer())?;
-		staging_memory.set_data(data)?;
+	pub fn new(device: Arc<VulkanDevice>, size: VkDeviceSize, data: Option<*const c_void>, usage: VkBufferUsageFlags) -> Result<Self, VulkanError> {
 		let buffer = VulkanBuffer::new(device.clone(), size, usage | VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_DST_BIT as u32)?;
 		let memory = VulkanMemory::new(device.clone(), &buffer.get_memory_requirements()?,
 			VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT as u32)?;
 		memory.bind_buffer(buffer.get_vk_buffer())?;
-		let mut command_pool = VulkanCommandPool::new(device.clone(), 1)?;
-		let pool_in_use = command_pool.use_pool(queue_index, None, true)?;
-		let copy_region = VkBufferCopy {
-			srcOffset: 0,
-			dstOffset: 0,
-			size,
-		};
-		vkcore.vkCmdCopyBuffer(pool_in_use.cmdbuf, staging_buffer.get_vk_buffer(), buffer.get_vk_buffer(), 1, &copy_region)?;
-		let submit_fence = pool_in_use.submit_fence.clone();
-		drop(pool_in_use);
-		submit_fence.wait(u64::MAX)?;
-		Ok(Self {
+		let mut ret = Self {
 			device,
 			memory,
 			buffer,
 			usage,
-		})
+		};
+		if let Some(data) = data {
+			ret.set_data(data, 0, size)?;
+		}
+		Ok(ret)
 	}
 
 	/// Update new data to the buffer
-	pub fn set_data(&self, data: *const c_void, offset: VkDeviceSize, length: VkDeviceSize, queue_index: usize) -> Result<(), VulkanError> {
+	pub fn set_data(&self, data: *const c_void, offset: VkDeviceSize, length: VkDeviceSize) -> Result<(), VulkanError> {
 		let vkcore = self.device.vkcore.clone();
 		let staging_buffer = VulkanBuffer::new(self.device.clone(), length, VkBufferUsageFlagBits::VK_BUFFER_USAGE_TRANSFER_SRC_BIT as u32)?;
 		let staging_memory = VulkanMemory::new(self.device.clone(), &staging_buffer.get_memory_requirements()?,
@@ -64,16 +50,12 @@ impl Buffer {
 		staging_memory.bind_buffer(staging_buffer.get_vk_buffer())?;
 		staging_memory.set_data(data)?;
 		let mut command_pool = VulkanCommandPool::new(self.device.clone(), 1)?;
-		let pool_in_use = command_pool.use_pool(queue_index, None, true)?;
 		let copy_region = VkBufferCopy {
 			srcOffset: 0,
 			dstOffset: offset,
 			size: length as VkDeviceSize,
 		};
 		vkcore.vkCmdCopyBuffer(pool_in_use.cmdbuf, staging_buffer.get_vk_buffer(), self.buffer.get_vk_buffer(), 1, &copy_region)?;
-		let submit_fence = pool_in_use.submit_fence.clone();
-		drop(pool_in_use);
-		submit_fence.wait(u64::MAX)?;
 		Ok(())
 	}
 }
