e34b694374031e6ddcc3d22b954330e681d78ad5
0xAA55 <weixiaobao213@qq.com>
2025-09-08 19:29:38 +0800

    同步用的部件需要在设备没有在使用它的时候，再被销毁。

--- src/basics.rs
+++ src/basics.rs
@@ -199,6 +199,7 @@ impl Debug for VulkanSemaphore {
 impl Drop for VulkanSemaphore {
 	fn drop(&mut self) {
 		let vkcore = self.device.vkcore.clone();
+		self.device.wait_idle().unwrap();
 		vkcore.vkDestroySemaphore(self.device.get_vk_device(), self.semaphore, null()).unwrap();
 	}
 }
@@ -344,6 +345,7 @@ impl Debug for VulkanFence {
 impl Drop for VulkanFence {
 	fn drop(&mut self) {
 		let vkcore = self.device.vkcore.clone();
+		self.device.wait_idle().unwrap();
 		vkcore.vkDestroyFence(self.device.get_vk_device(), self.fence, null()).unwrap();
 	}
 }
