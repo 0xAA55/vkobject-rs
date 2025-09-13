d224cdbabc92702d7f5cc5912842791f1b99e7c9
0xAA55 <weixiaobao213@qq.com>
2025-09-13 17:40:51 +0800

    实现从文件或者从字符串编译着色器源码。

--- src/shader.rs
+++ src/shader.rs
@@ -18,6 +18,28 @@ pub struct VulkanShader {
 	shader: VkShaderModule,
 }
 
+/// The shader source
+#[derive(Debug, Clone, Copy)]
+pub enum ShaderSource<'a> {
+	VertexShader(&'a str),
+	GeometryShader(&'a str),
+	FragmentShader(&'a str),
+	ComputeShader(&'a str)
+}
+
+/// The shader source path
+#[derive(Debug, Clone, Copy)]
+pub enum ShaderSourcePath<'a> {
+	VertexShader(&'a Path),
+	GeometryShader(&'a Path),
+	FragmentShader(&'a Path),
+	ComputeShader(&'a Path)
+}
+
+/// The optimization level for shaderc
+#[cfg(feature = "shaderc")]
+pub use shaderc::OptimizationLevel;
+
 impl VulkanShader {
 	/// Create the `VulkanShader` from the shader code, it should be aligned to 32-bits
 	pub fn new(device: Arc<VulkanDevice>, shader_code: &[u32]) -> Result<Self, VulkanError> {
@@ -48,6 +70,38 @@ impl VulkanShader {
 			Vec::from_raw_parts(ptr, len, cap)
 		};
 		Self::new(device, &shader_code)
+	}
+
+	/// Create the `VulkanShader` from source code
+	#[cfg(feature = "shaderc")]
+	pub fn new_from_source(device: Arc<VulkanDevice>, code: ShaderSource, filename: &str, entry_point: &str, level: OptimizationLevel, debug_info: bool, warning_as_error: bool) -> Result<Self, VulkanError> {
+		use shaderc::*;
+		use ShaderSource::*;
+		let compiler = Compiler::new()?;
+		let mut options = CompileOptions::new()?;
+		options.set_optimization_level(level);
+		if debug_info {options.set_generate_debug_info()}
+		if warning_as_error {options.set_warnings_as_errors()}
+		options.set_target_env(TargetEnv::Vulkan, device.vkcore.get_app_info().apiVersion);
+		let artifact = match code {
+			VertexShader(source) => compiler.compile_into_spirv(source, ShaderKind::Vertex, filename, entry_point, Some(&options))?,
+			GeometryShader(source) => compiler.compile_into_spirv(source, ShaderKind::Geometry, filename, entry_point, Some(&options))?,
+			FragmentShader(source) => compiler.compile_into_spirv(source, ShaderKind::Fragment, filename, entry_point, Some(&options))?,
+			ComputeShader(source) => compiler.compile_into_spirv(source, ShaderKind::Compute, filename, entry_point, Some(&options))?,
+		};
+		Self::new(device, artifact.as_binary())
+	}
+
+	/// Create the `VulkanShader` from source code
+	#[cfg(feature = "shaderc")]
+	pub fn new_from_source_file(device: Arc<VulkanDevice>, code_path: ShaderSourcePath, entry_point: &str, level: OptimizationLevel, debug_info: bool, warning_as_error: bool) -> Result<Self, VulkanError> {
+		use ShaderSourcePath::*;
+		match code_path {
+			VertexShader(path) =>   {let bytes = read(path)?; let source = unsafe {str::from_utf8_unchecked(&bytes)}; Self::new_from_source(device, ShaderSource::VertexShader(source),   &path.file_name().unwrap().to_string_lossy().to_owned(), entry_point, level, debug_info, warning_as_error)}
+			GeometryShader(path) => {let bytes = read(path)?; let source = unsafe {str::from_utf8_unchecked(&bytes)}; Self::new_from_source(device, ShaderSource::GeometryShader(source), &path.file_name().unwrap().to_string_lossy().to_owned(), entry_point, level, debug_info, warning_as_error)}
+			FragmentShader(path) => {let bytes = read(path)?; let source = unsafe {str::from_utf8_unchecked(&bytes)}; Self::new_from_source(device, ShaderSource::FragmentShader(source), &path.file_name().unwrap().to_string_lossy().to_owned(), entry_point, level, debug_info, warning_as_error)}
+			ComputeShader(path) =>  {let bytes = read(path)?; let source = unsafe {str::from_utf8_unchecked(&bytes)}; Self::new_from_source(device, ShaderSource::ComputeShader(source),  &path.file_name().unwrap().to_string_lossy().to_owned(), entry_point, level, debug_info, warning_as_error)}
+		}
 	}
 }
 
