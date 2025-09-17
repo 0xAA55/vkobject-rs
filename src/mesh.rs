7a68a308d880bab145d3a83b47beb39bab9cdeb6
0xAA55 <weixiaobao213@qq.com>
2025-09-17 22:16:44 +0800

    我要提交代码了。

--- src/mesh.rs
+++ src/mesh.rs
@@ -3,12 +3,7 @@ use crate::prelude::*;
 use struct_iterable::Iterable;
 
 #[derive(Debug, Clone)]
-pub struct Mesh<V: BufferVecItem, E: BufferVecItem, I: BufferVecItem, C: BufferVecItem> {
 	pub primitive_type: VkPrimitiveTopology,
-	pub vertices: BufferVec<V>,
-	pub indices: Option<BufferVec<E>>,
-	pub instances: Option<BufferVec<I>>,
-	pub commands: Option<BufferVec<C>>,
 }
 
 #[derive(Default, Debug, Clone, Copy, Iterable)]
@@ -20,13 +15,11 @@ pub fn buffer_unused() -> Option<UnusedBufferType> {
 	None
 }
 
-impl<V, E, I, C> Mesh<V, E, I, C>
 where
 	V: BufferVecItem,
 	E: BufferVecItem,
 	I: BufferVecItem,
 	C: BufferVecItem {
-	pub fn new(primitive_type: VkPrimitiveTopology, vertices: BufferVec<V>, indices: Option<BufferVec<E>>, instances: Option<BufferVec<I>>, commands: Option<BufferVec<C>>) -> Self {
 		Self {
 			primitive_type,
 			vertices,
@@ -36,7 +29,5 @@ where
 		}
 	}
 
-	pub fn strip_vertices_staging_buffer(&mut self) -> Result<(), VulkanError> {
-		unsafe {self.vertices.change_capacity(0)}
 	}
 }
