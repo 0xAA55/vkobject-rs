356643e336da683be82a3e842eeac75921b53ba9
0xAA55 <weixiaobao213@qq.com>
2025-10-08 19:28:11 +0800

    不需要材质名字了，省略之。

--- src/lib.rs
+++ src/lib.rs
@@ -409,7 +409,7 @@ mod tests {
 
 				scene.set_viewport_swapchain(0.0, 1.0)?;
 				scene.set_scissor_swapchain()?;
-				for (matname, pipeline) in self.pipelines.iter() {
+				for pipeline in self.pipelines.values() {
 					scene.begin_renderpass(Vec4::new(0.0, 0.0, 0.2, 1.0), 1.0, 0)?;
 					pipeline.draw(cmdbuf)?;
 					scene.end_renderpass()?;
