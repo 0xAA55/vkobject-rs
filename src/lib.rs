be73dccdfd08c8e809486b2737765fdb86f7b2d9
0xAA55 <weixiaobao213@qq.com>
2025-10-21 21:58:26 +0800

    给 `UniformBuffer` 提供默认值参数。

--- src/lib.rs
+++ src/lib.rs
@@ -264,7 +264,7 @@ mod tests {
 					None,
 					Arc::new(VulkanShader::new_from_source_file_or_cache(device.clone(), ShaderSourcePath::FragmentShader(PathBuf::from("shaders/test.fsh")), false, "main", OptimizationLevel::Performance, false)?),
 				));
-				let uniform_input: Arc<dyn GenericUniformBuffer> = Arc::new(UniformBuffer::<UniformInput>::new(device.clone())?);
+				let uniform_input: Arc<dyn GenericUniformBuffer> = Arc::new(UniformBuffer::<UniformInput>::new(device.clone(), None)?);
 				let desc_props = Arc::new(DescriptorProps::default());
 				desc_props.new_uniform_buffer(0, 0, uniform_input.clone());
 				let pool_in_use = ctx.cmdpools[0].use_pool(None)?;
@@ -369,7 +369,7 @@ mod tests {
 				));
 				let pool_in_use = ctx.cmdpools[0].use_pool(None)?;
 				let object = GenericMeshSet::create_meshset_from_obj_file::<f32, ObjVertPositionTexcoord2DNormalTangent, _>(device.clone(), "assets/testobj/avocado.obj", pool_in_use.cmdbuf, Some(&[InstanceType {transform: Mat4::identity()}; Self::OBJ_ROWS * Self::OBJ_COLS]))?;
-				let uniform_input_scene: Arc<dyn GenericUniformBuffer> = Arc::new(UniformBuffer::<UniformInputScene>::new(device.clone())?);
+				let uniform_input_scene: Arc<dyn GenericUniformBuffer> = Arc::new(UniformBuffer::<UniformInputScene>::new(device.clone(), None)?);
 				let desc_props = Arc::new(DescriptorProps::default());
 				desc_props.new_uniform_buffer(0, 0, uniform_input_scene.clone());
 				let mut pipelines: HashMap<String, Pipeline> = HashMap::with_capacity(object.meshset.len());
